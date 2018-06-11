145cc72a32da488ca5c15c3bcc36534f4de2b404
Alok Kumar <alok760@gmail.com>
2018-06-12 02:35:21 +0530

    updating database name

--- new.php
+++ new.php
@@ -61,7 +61,7 @@
             $servername = "localhost";
             $username = "root";
             $password = "";
-            $dbname = "soumya";
+            $dbname = "health";
 
             // Create connection
             $conn = new mysqli($servername, $username, $password, $dbname);
