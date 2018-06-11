656235f27c86072a0b60e92a24603eb82c62f13a
Alok Kumar <alok760@gmail.com>
2018-06-12 02:46:26 +0530

    create table php to automatically create the required initial table

---
+++ create_table.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "health";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$sql = "CREATE TABLE main ( name varchar(20) NOT NULL, age int(4) NOT NULL, gender varchar(20) NOT NULL, id varchar(20) NOT NULL, PRIMARY KEY (id) ) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
