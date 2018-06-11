4d76758a5cf76a6750eace3de072fafaf8294e14
Alok Kumar <alok760@gmail.com>
2018-06-12 02:52:32 +0530

    correcting table name in create table php

--- create_table.php
+++ create_table.php
@@ -15,7 +15,7 @@ if ($conn->connect_error) {
 $sql = "CREATE TABLE main ( name varchar(20) NOT NULL, age int(4) NOT NULL, gender varchar(20) NOT NULL, id varchar(20) NOT NULL, PRIMARY KEY (id) ) ENGINE=InnoDB DEFAULT CHARSET=latin1";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Table MyGuests created successfully";
+    echo "Table main created successfully";
 } else {
     echo "Error creating table: " . $conn->error;
 }
